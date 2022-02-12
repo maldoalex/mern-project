344d132bddbedc94f2b2a85f0b98aacb5661d1cf
maldoalex <maldoalex@gmail.com>
2022-02-12 17:21:13 -0600

    added google maps to modal

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,12 +3,15 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-  const USERS = [{
-    id: 'u1',
-    name: 'Alex Maldonado',
-    image: 'https://images.unsplash.com/photo-1542931287-023b922fa89b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHRva3lvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
-    moments: 3,
-  }];
+  const USERS = [
+    {
+      id: "u1",
+      name: "Alex Maldonado",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEkAO3NjmrhH75wWrcfNGZ66reLDY4xqIrXg&usqp=CAU",
+      moments: 3,
+    },
+  ];
 
   return (
     <UsersList items={USERS} />
