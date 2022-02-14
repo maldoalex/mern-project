5e38568b68f82746d7a653147a56315f848569c4
maldoalex <maldoalex@gmail.com>
2022-02-14 10:41:56 -0600

    useReducer to handle form in newMomentcomp

--- src/moments/pages/NewMoment.js
+++ src/moments/pages/NewMoment.js
@@ -1,16 +1,61 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useReducer} from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/Validators';
 import './NewMoment.css'
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE': 
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+        ...state.inputs,
+        [action.inputId]: {value: action.value, isValid: action.isValid}
+        },
+        isValid: formIsValid
+      };
+        default:
+      return state;
+  }
+};
+
 const NewMoment = () => {
-  const titleInputHandler = useCallback((id, value, isValid) => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+      isValid: false
+  });
 
-  },[]);
-  const descriptionInputHandler = useCallback((id, value, isValid) => {
 
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE', 
+      value: value, 
+      isValid: isValid, 
+      inputId: id
+    })
   },[]);
+
+  // const descriptionInputHandler = useCallback((id, value, isValid) => {
+  // },[]);
 
   return (
     <form className='moment-form'>
@@ -21,7 +66,7 @@ const NewMoment = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
-        onInput={titleInputHandler}
+        onInput={inputHandler}
        />
       <Input 
         id="description"
@@ -29,8 +74,9 @@ const NewMoment = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 charcters)."
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
        />
+      <Button type="submit" disabled={!formState.isValid}>ADD MOMENT</Button>
     </form>
   )  
 };
