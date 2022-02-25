ce84a7010f51306d59e514c67738b10bd3afd4b2
maldoalex <maldoalex@gmail.com>
2022-02-24 18:41:42 -0600

    image upload for user and moment in frontend

--- src/moments/components/MomentItem.js
+++ src/moments/components/MomentItem.js
@@ -72,7 +72,7 @@ const MomentItem = props => {
         <Card className="moment-item__content">
         {isLoading && <LoadingSpinner asOverlay />}
           <div className="moment-item__image">
-            <img src={props.image} alt={props.title} />
+            <img src={`http://localhost:5000/${props.image}`} alt={props.title} />
           </div>
           <div className="moment-item__info">
             <h2>{props.title}</h2>
